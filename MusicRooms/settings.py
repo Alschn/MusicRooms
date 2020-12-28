846a31ceecd8199d26a362be4249a27d2737fc53
Alschn <adam.lisichin@gmail.com>
2020-12-28 18:11:44 +0100

    Setup Django+React integration

--- MusicRooms/settings.py
+++ MusicRooms/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'frontend.apps.FrontendConfig',
     'rest_framework',
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,9 @@
-from frontend.views import index
 from django.urls import path
+from frontend.views import index
 
 urlpatterns = [
     path('', index),
+    path('join', index),
+    path('create', index),
+    path('room/<str:roomCode>', index),
 ]
